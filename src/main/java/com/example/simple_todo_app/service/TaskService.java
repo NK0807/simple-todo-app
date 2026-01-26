413e3086c348c0752313fee3cbeae7b4a41605ba
NK0807 <hyue3434@gmail.com>
2026-01-26 03:40:43 +0000

    削除機能を追加

--- src/main/java/com/example/simple_todo_app/service/TaskService.java
+++ src/main/java/com/example/simple_todo_app/service/TaskService.java
@@ -41,4 +41,9 @@ public class TaskService {
             repository.save(task);
         }
     }
+
+    // 削除する機能
+    public void delete(Integer id) {
+        repository.deleteById(id);
+    }
 }
